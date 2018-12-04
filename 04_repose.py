791cc9f619ffadf9e49ec49975da84308f949c3c
Geoffrey Spear <geoffspear@gmail.com>
2018-12-04 07:40:24 -0500

    day 4 solution

--- 04_repose.py
+++ 04_repose.py
@@ -1,34 +1,7 @@
 import collections
 import dataclasses
 
-@dataclasses.dataclass
-class Guard:
-    total_sleep: int = 0
-    hours_slept: collections.Counter = dataclasses.field(default_factory=collections.Counter)
-    
-
-def findtime(data):
-    guards = collections.defaultdict(Guard)
-    current_guard = None
-    data_iter = iter(data)
-    for line in data_iter:
-        if "Guard #" in line:
-            current_guard = int(line.partition("Guard #")[2].split()[0])
-            continue
-        assert "falls asleep" in line
-        start_time = int(line.partition(":")[2][:2])
-        line2 = next(data_iter)
-        assert "wakes up" in line2
-        end_time = int(line2.partition(":")[2][:2])
-        guard = guards[current_guard]
-        for t in range(start_time, end_time):
-            guard.total_sleep += 1
-            guard.hours_slept[t] += 1
-    print(guards)
-    
-
-def test_findtime():
-    data = """[1518-11-01 00:00] Guard #10 begins shift
+TEST_DATA = """[1518-11-01 00:00] Guard #10 begins shift
 [1518-11-01 00:05] falls asleep
 [1518-11-01 00:25] wakes up
 [1518-11-01 00:30] falls asleep
@@ -46,9 +19,54 @@ def test_findtime():
 [1518-11-05 00:45] falls asleep
 [1518-11-05 00:55] wakes up""".splitlines()
 
-    assert findtime(data) == 240
 
+@dataclasses.dataclass
+class Guard:
+    total_sleep: int = 0
+    hours_slept: collections.Counter = dataclasses.field(default_factory=collections.Counter)
+    
+
+def make_guards(data):
+    guards = collections.defaultdict(Guard)
+    current_guard = None
+    data_iter = iter(data)
+    for line in data_iter:
+        if "Guard #" in line:
+            current_guard = int(line.partition("Guard #")[2].split()[0])
+            continue
+        assert "falls asleep" in line
+        start_time = int(line.partition(":")[2][:2])
+        line2 = next(data_iter)
+        assert "wakes up" in line2
+        end_time = int(line2.partition(":")[2][:2])
+        guard = guards[current_guard]
+        for t in range(start_time, end_time):
+            guard.total_sleep += 1
+            guard.hours_slept[t] += 1
+    return guards
+
+def most_freq(data):
+    guards = make_guards(data)        
+    chosen_guard = max(guards.items(), key=lambda item: item[1].hours_slept.most_common(1)[0][1])
+    return chosen_guard[0] * chosen_guard[1].hours_slept.most_common(1)[0][0]
+
+    
+def findtime(data):            
+    guards = make_guards(data)        
+    chosen_guard = max(guards.items(), key=lambda item: item[1].total_sleep)
+    return chosen_guard[0] * chosen_guard[1].hours_slept.most_common(1)[0][0]
+
+
+def test_findtime():
+    assert findtime(TEST_DATA) == 240
+
+
+def test_most_freq():
+    assert most_freq(TEST_DATA) == 4455
+    
 
 if __name__ == "__main__":
     with open('04_input.txt') as f:
         print(findtime(sorted(f)))
+        f.seek(0)
+        print(most_freq(sorted(f)))
